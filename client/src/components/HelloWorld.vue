35cc96d8bee0723e74a2627566cc45dd6708dd85
HoomanBoy <hoo.nl@outlook.com>
2021-02-21 23:37:49 +0100

    De app is launch

---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <article class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Hier is client side of my app'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
